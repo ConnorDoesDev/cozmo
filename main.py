dc2e4574d056b6e61e1e7042c3b32d7e0d00f055
Connor <halil_ismail@yahoo.com>
2021-11-04 18:01:34 +0100

    updated main file

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import cozmo
 
+name = input("What is your name? ")
+
 
 def cozmo_program(robot: cozmo.robot.Robot):
-    robot.say_text("My name is Cozmo, what about you and why did you help me?").wait_for_completed()
+    robot.say_text(
+        f"Hi! My name is Cozmo. How are you, {name}?").wait_for_completed()
 
 
 cozmo.run_program(cozmo_program)
